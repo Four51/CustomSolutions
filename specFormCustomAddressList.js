905cba8531a0cd2beaa6e82e26f971f73168520a
jenrasmussen <jrasmussen@four51.com>
2014-08-13 16:08:24 -0500

    Update specFormCustomAddressList.js

--- specFormCustomAddressList.js
+++ specFormCustomAddressList.js
@@ -7,8 +7,7 @@ fields attached to an order for buyers with 100+ addresses) updated for spec for
 click out of field) for optional editing and saving.
 - A prepopulated address book address that is modified will not update the address in the user address book. 
 */
-
-
+=================================================================================
 
 /* add to specFormCtrl.js and modify Variant Spec names to match Variable Specs */
 
