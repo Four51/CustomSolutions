c2cdcc74066edc73c2a6bb5f09335d0eb7d3d633
jenrasmussen <jrasmussen@four51.com>
2014-11-04 17:43:18 -0600

    Update productDisplayService.js

--- bootstrapLightbox/productDisplayService.js
+++ bootstrapLightbox/productDisplayService.js
@@ -1,50 +1,50 @@
 /*Add the below snippet to js/services/productDisplayService.js within function productViewScope(scope){} 
 and after the scope.setAddToOrderErrors = function(){}*/
 	
-	    if (scope.LineItem.Product.StaticSpecGroups) {
-		
-			if (scope.LineItem.Product.StaticSpecGroups.GalleryImages) {
-		
-			scope.LineItem.images = [];
-			var count = 1;
+if (scope.LineItem.Product.StaticSpecGroups) {
 
-				angular.forEach (scope.LineItem.Product.StaticSpecGroups.GalleryImages.Specs, function(spec) {
-					var image = {};
-					image.url = spec.FileURL;
-					image.thumbUrl = scope.LineItem.Product.StaticSpecGroups.GalleryThumbs.Specs[count].FileURL;
-					image.Number = count;
-					scope.LineItem.images.push(image);
-					count++;
-				});
-			}
+	if (scope.LineItem.Product.StaticSpecGroups.GalleryImages) {
+
+	scope.LineItem.images = [];
+	var count = 1;
+
+		angular.forEach (scope.LineItem.Product.StaticSpecGroups.GalleryImages.Specs, function(spec) {
+			var image = {};
+			image.url = spec.FileURL;
+			image.thumbUrl = scope.LineItem.Product.StaticSpecGroups.GalleryThumbs.Specs[count].FileURL;
+			image.Number = count;
+			scope.LineItem.images.push(image);
+			count++;
+		});
+	}
+}
+
+if (scope.LineItem.Specs) {
+
+	if (scope.LineItem.Specs.Color) {
+		 
+		scope.LineItem.images = [];
+		var count = 1;
+	
+		var color = scope.LineItem.Specs.Color.Value;
+		var colorLarge = color + 'Large';
+		var colorSmall = color + 'Small';
+		
+		if (scope.LineItem.Product.StaticSpecGroups[colorLarge]) {
+
+			angular.forEach (scope.LineItem.Product.StaticSpecGroups[colorLarge].Specs, function(spec) {
+				var image = {};
+				image.url = spec.FileURL;
+				image.thumbUrl = scope.LineItem.Product.StaticSpecGroups[colorSmall].Specs[count].FileURL;
+				image.Number = count;
+				scope.LineItem.images.push(image);
+				count++;
+			});
 		}
 		
-		if (scope.LineItem.Specs) {
-		
-			if (scope.LineItem.Specs.Color) {
-				 
-				scope.LineItem.images = [];
-				var count = 1;
-			
-				var color = scope.LineItem.Specs.Color.Value;
-				var colorLarge = color + 'Large';
-				var colorSmall = color + 'Small';
-				
-				if (scope.LineItem.Product.StaticSpecGroups[colorLarge]) {
+	scope.imageLoaded = true;
+	}
+}
 
-					angular.forEach (scope.LineItem.Product.StaticSpecGroups[colorLarge].Specs, function(spec) {
-						var image = {};
-						image.url = spec.FileURL;
-						image.thumbUrl = scope.LineItem.Product.StaticSpecGroups[colorSmall].Specs[count].FileURL;
-						image.Number = count;
-						scope.LineItem.images.push(image);
-						count++;
-					});
-				}
-				
-			scope.imageLoaded = true;
-			}
-		}
-
-        //trigger the click for the first image (gives the large image)
-        scope.index = 0;
+//trigger the click for the first image (gives the large image)
+scope.index = 0;
