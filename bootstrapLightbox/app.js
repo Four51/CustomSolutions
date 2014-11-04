7b841d8364898c42ef057bb82eff42e7ee592436
jenrasmussen <jrasmussen@four51.com>
2014-11-04 17:48:16 -0600

    Update app.js

--- bootstrapLightbox/app.js
+++ bootstrapLightbox/app.js
@@ -1,4 +1,11 @@
+/***  
+js/app.js Rev: 10/8/14 
+Inject the bootstrapLightbox module
+***/
+
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'bootstrapLightbox']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 
+'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 
+'ngAutocomplete', 'bootstrapLightbox']);
