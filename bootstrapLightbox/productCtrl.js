915f02a6a338b077924add3ec0bf62fa1cb7c834
jenrasmussen <jrasmussen@four51.com>
2014-12-08 12:29:45 -0600

    Create productCtrl.js

---
+++ bootstrapLightbox/productCtrl.js
@@ -0,0 +1,14 @@
+/* Inject 'Lightbox' into js/controllers/productCtrl.js and add $scope.$watch and $scope.openLightboxModal fn */
+	
+	
+	$scope.$watch('LineItem.Specs.Color.Value', function(n,o){
+		//if ( n!= o && $scope.imageLoaded == 'true') {
+		if ( n!= o) {
+			ProductDisplayService.setProductViewScope($scope);
+			Lightbox.setImages($scope.LineItem.images);
+		}
+	});
+	
+		$scope.openLightboxModal = function (index) {
+	Lightbox.openModal($scope.LineItem.images, index);
+	};
