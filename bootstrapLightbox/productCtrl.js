ead3cad205b88b9380901cb309b2e621bd190a9e
jenrasmussen <jrasmussen@four51.com>
2015-01-10 14:18:06 -0600

    Update productCtrl.js

--- bootstrapLightbox/productCtrl.js
+++ bootstrapLightbox/productCtrl.js
@@ -1,6 +1,10 @@
 /* Inject 'Lightbox' into js/controllers/productCtrl.js and add $scope.$watch and $scope.openLightboxModal fn */
-	
-	
+
+/* Injection */
+four51.app.controller('ProductCtrl', ['$scope', '$routeParams', '$route', '$location', '$451', 'Product', 'ProductDisplayService', 'Order', 'Variant', 'User', 'Lightbox',
+    function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplayService, Order, Variant, User, Lightbox) {
+    	
+/*functions*/
 	$scope.$watch('LineItem.Specs.Color.Value', function(n,o){
 		if ( n!= o) {
 			ProductDisplayService.setProductViewScope($scope);
