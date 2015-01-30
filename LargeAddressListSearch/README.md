67643471903a0d3a3efb5398cedda0df2e8732cc
Kyle Olson <kolson@four51.com>
2015-01-30 10:29:25 -0600

    Large Address List Search solution

---
+++ LargeAddressListSearch/README.md
@@ -0,0 +1,92 @@
+#Large Address Solution for OrderCloud
+
+This is an overview of how to implement the Large Address List Search solution for an OrderCloud 2.0 site. This solution can be used if a buyer site has more than 100 shipping and/or billing addresses. Since the API limits results to 100, a site that has more shipping/billing addresses than this will not see their entire list. This solution allows the user to search for their desired address from the large list, without obtaining all of those addresses before hand.
+
+##Setup
+###1. Add largeaddresslistsearch.js file to your project.
+
+If you are using a repository for your project, add this file to the **`/lib/angular/plugins`** directory and also add that path to the **`index.html`** file.
+
+If you are using file overrides for your project, create a new file override named **`/lib/angular/largeaddresslistsearch.js`**, add this file as the content for that override, then add the file reference to the **`index.html`** file override.
+
+###2. Remove AddressList API calls in **`/js/directives/ordershipping.js`** and/or **`/js/directives/orderbilling.js`**
+
+If you are adding this solution to the shipping section, comment out or remove the following from **`/js/directives/ordershipping.js`**:
+
+```javascript
+    AddressList.clear();
+    AddressList.shipping(function(list) {
+        $scope.shipaddresses = list;
+        if ($scope.isEditforApproval) {
+            if (!AddressList.contains($scope.currentOrder.ShipAddress))
+                $scope.shipaddresses.push($scope.currentOrder.ShipAddress);
+        }
+    });
+``` 
+
+If you are adding this solution to the billing section, comment out or remove the following from **`/js/directives/orderbilling.js`**:
+
+```javascript
+    AddressList.clear();
+    AddressList.billing(function(list) {
+    $scope.billaddresses = list;
+    if ($scope.isEditforApproval) {
+        if (!AddressList.contains($scope.currentOrder.BillAddress))
+            $scope.billaddresses.push($scope.currentOrder.BillAddress);
+    }
+    });
+``` 
+
+These calls are removed in order to avoid excess API calls that do not need to occur.
+
+###3. Replace original address controls with new directives
+
+If you are adding this solution to the shipping section, comment out or remove the following from **`/partials/controls/orderShipping.html`**:
+
+```html
+    <div ng-show="shipaddresses" ng-class="{'view-form-select': !currentOrder.ShipAddressID, '': currentOrder.ShipAddressID }">
+        <label ng-class="{required: !currentOrder.IsMultipleShip()}" ng-show="currentOrder.ShipAddressID || !currentOrder.IsMultipleShip()">{{('Shipping' | r) + ' ' + ('Address' | r) | xlat}}</label>
+            <select class="form-control" name="shippingAddress"
+                    ng-change="setShipAddressAtOrderLevel()"
+                    ng-options="address.ID as address.AddressName for address in shipaddresses"
+                    ng-model="currentOrder.ShipAddressID"
+                    ng-required="!currentOrder.IsMultipleShip()">
+                <option value=""></option>
+            </select>
+            <i class="fa fa-map-marker"></i>
+    </div>
+```
+
+and replace it with:
+
+```html
+    <largeaddressshipping></largeaddressshipping>
+```
+
+If you are adding this solution to the billing section, comment out or remove the following from **`/partials/controls/orderBilling.html`**:
+
+```html
+    <div class="view-form-icon" ng-show="billaddresses.length > 0">
+        <div ng-class="{'view-form-select': !currentOrder.BillAddressID, '': currentOrder.BillAddressID }">
+            <label class="required">{{('Billing' | r) + ' ' + ('Address' | r) | xlat}}</label>
+            <select class="form-control" ng-show="billaddresses" name="billingAddress"
+                    ng-options="address.ID as address.AddressName for address in billaddresses | filter:{IsBilling:true}" ng-model="currentOrder.BillAddressID" required>
+                <option value=""></option>
+            </select>
+            <i class="fa fa-map-marker"></i>
+        </div>
+    </div>
+```
+
+and replace it with:
+
+```html
+    <largeaddressbilling></largeaddressbilling>
+```
+
+If you would like to change what displays within the typeahead dropdown when searching for addresses, adjust the "as" portion of the typeahead attribute on the directive's template HTML. For example, if you'd only like the Address Line 1 to display for shipping addresses, the typeahead attribute would read:
+
+```html
+    typeahead="address as (address.Street1) for address in shipaddresses"
+```
+
---
+++ LargeAddressListSearch/largeaddresslistsearch.js
@@ -0,0 +1,144 @@
+angular.module('OrderCloud-LargeAddressListSearch', []);
+
+angular.module('OrderCloud-LargeAddressListSearch')
+
+    .factory('LargeAddress', largeaddress)
+    .directive('largeaddressshipping', largeaddresssearchshipping)
+    .directive('largeaddressbilling', largeaddresssearchbilling)
+    .controller('LargeShipAddressCtrl', largeaddressshippingctrl)
+    .controller('LargeBillAddressCtrl', largeaddressbillingctrl);
+
+    largeaddress.$inject = ['$resource', '$451'];
+
+    function largeaddress($resource, $451) {
+        var service = {
+            queryShipping: _queryShipping,
+            queryBilling: _queryBilling
+        };
+        return service;
+
+        function _queryShipping(searchTerm, success) {
+            $resource($451.api('address/shipping')).get({ editable: false, key: searchTerm, page: 1, pagesize: 100}).$promise.then(function (list) {
+                success(list.List, list.Count);
+            });
+        }
+
+        function _queryBilling(searchTerm, success) {
+            $resource($451.api('address/billing')).get({ editable: false, key: searchTerm, page: 1, pagesize: 100}).$promise.then(function (list) {
+                success(list.List, list.Count);
+            });
+        }
+    }
+
+    function largeaddresssearchshipping() {
+        var obj = {
+            restrict: 'E',
+            controller: 'LargeShipAddressCtrl',
+            template:   '<div class="row">' +
+                            '<div class="col-md-6">' +
+                                '<div class="view-form-icon">' +
+                                    '<div class="form-group">' +
+                                        '<label>{{("Shipping" | r) + " " + ("Address" | r) | xlat}}</label>' +
+                                        '<input class="form-control" type="text" ng-model="ShipAddress" ng-change="searchShipAddresses(ShipAddress)" typeahead="address as (address.AddressName + \' \' + (address.FirstName || \'\') + \' \' + (address.LastName || \'\') + \' \' + (address.Street1 || \'\') + \' \' + (address.Street2 || \'\') + \' \' + (address.City || \'\') + \' \' + (address.State || \'\') + \' \' + (address.Zip || \'\')) for address in shipaddresses" typeahead-min-length="3" />' +
+                                        '<i class="fa fa-map-marker"></i>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>'
+        };
+        return obj;
+    }
+
+    largeaddressshippingctrl.$inject = ['$scope', 'LargeAddress', 'Address'];
+
+    function largeaddressshippingctrl($scope, LargeAddress, Address) {
+        $scope.$watch('ShipAddress', function(newValue) {
+            if (!newValue || !newValue.ID) return;
+            $scope.orderShipAddress = newValue;
+            $scope.currentOrder.ShipAddress = newValue;
+            if ($scope.currentOrder) {
+                $scope.currentOrder.ShipAddressID = newValue.ID;
+                $scope.currentOrder.ShipFirstName = null;
+                $scope.currentOrder.ShipLastName = null;
+                angular.forEach($scope.currentOrder.LineItems, function(item) {
+                    item.ShipFirstName = null;
+                    item.ShipLastName = null;
+                });
+            }
+
+            if (newValue) {
+                if ($scope.user.Permissions.contains('EditShipToName') && !newValue.IsCustEditable) {
+                    angular.forEach($scope.currentOrder.LineItems, function(item) {
+                        item.ShipFirstName = newValue.FirstName;
+                        item.ShipLastName = newValue.LastName;
+                    });
+                }
+            }
+            $scope.setShipAddressAtOrderLevel();
+        });
+
+        $scope.searchShipAddresses = function(searchTerm) {
+            if (searchTerm && searchTerm.length > 2) {
+                $scope.shipaddresses = [' '];
+                LargeAddress.queryShipping(searchTerm, function(list, count) {
+                    $scope.shipaddresses = list;
+                });
+            }
+        };
+
+        if ($scope.currentOrder.ShipAddressID) {
+            Address.get($scope.currentOrder.ShipAddressID, function(add) {
+                $scope.ShipAddress = add;
+            });
+        }
+    }
+
+    function largeaddresssearchbilling() {
+        var obj = {
+            restrict: 'E',
+            controller: 'LargeBillAddressCtrl',
+            template:   '<div class="row">' +
+                            '<div class="col-md-6">' +
+                                '<div class="view-form-icon">' +
+                                    '<div class="form-group">' +
+                                        '<label>{{("Billing" | r) + " " + ("Address" | r) | xlat}}</label>' +
+                                        '<input class="form-control" type="text" ng-model="BillAddress" ng-change="searchBillAddresses(BillAddress)" typeahead="address as (address.AddressName + \' \' + (address.FirstName || \'\') + \' \' + (address.LastName || \'\') + \' \' + (address.Street1 || \'\') + \' \' + (address.Street2 || \'\') + \' \' + (address.City || \'\') + \' \' + (address.State || \'\') + \' \' + (address.Zip || \'\')) for address in billaddresses" typeahead-min-length="3" />' +
+                                        '<i class="fa fa-map-marker"></i>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>'
+        };
+        return obj;
+    }
+
+    largeaddressbillingctrl.$inject = ['$scope', 'LargeAddress'];
+
+    function largeaddressbillingctrl($scope, LargeAddress) {
+        $scope.$watch('BillAddress', function(newValue) {
+            if (!newValue || !newValue.ID) return;
+            $scope.orderBillAddress = newValue;
+            $scope.currentOrder.BillAddress = newValue;
+            if ($scope.user.Permissions.contains('EditBillToName') && !newValue.IsCustEditable) {
+                $scope.currentOrder.BillFirstName = newValue.FirstName;
+                $scope.currentOrder.BillLastName = newValue.LastName;
+            }
+            $scope.BillAddressID = newValue.ID;
+            $scope.BillAddress = newValue;
+        });
+
+        $scope.searchBillAddresses = function(searchTerm) {
+            if (searchTerm && searchTerm.length > 2) {
+                $scope.billaddresses = [' '];
+                LargeAddress.queryBilling(searchTerm, function(list, count) {
+                    $scope.billaddresses = list;
+                });
+            }
+        };
+
+        if ($scope.currentOrder.BillAddressID) {
+            Address.get($scope.currentOrder.BillAddressID, function(add) {
+                $scope.BillAddress = add;
+            });
+        }
+    }
