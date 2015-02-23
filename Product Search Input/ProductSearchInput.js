5072a19093f661c160189277c6dd1861d5bbdc46
Robert <rwatt@four51.com>
2015-02-23 17:14:11 -0600

    Product Search Input modulized and readme.MD added

---
+++ Product Search Input/ProductSearchInput.js
@@ -0,0 +1,70 @@
+//Add js file to lib/angular/plugins
+//Add reference to file to index.html
+//Add module as dependency to app.js
+
+angular.module('OrderCloud-ProductSearchInput', []);
+
+angular.module('OrderCloud-ProductSearchInput')
+
+	.directive('productsearchinput', function() {
+		var obj = {
+			restrict: 'E',
+			templateUrl:'productSearch.html',
+			controller: 'ProductSearchInputCtrl'
+		};
+		return obj;
+	})
+
+	.directive('productsearchinputnav', function() {
+		var obj = {
+			restrict: 'E',
+			templateUrl: 'productSearchNav.html',
+			controller: 'ProductSearchInputCtrl'
+		};
+		return obj;
+	})
+
+	.controller('ProductSearchInputCtrl', ['$scope','$location', function($scope,$location) {
+		$scope.displayProductSearch = false;
+		$scope.executeSearch = function() {
+			var searchTerm = $scope.productSearchTerm;
+			$scope.productSearchTerm = null;
+			$scope.displayProductSearch = false;
+			$location.path('search/' + searchTerm);
+		};
+	}])
+
+	.run(['$templateCache', function($templateCache) {
+		$templateCache.put('productSearch.html',
+				'<form name="productSearchInput" ng-submit="executeSearch()">' +
+				'<div class="view-form-icon">' +
+				'<div class="input-group" style="margin:0 0 10px;">' +
+				'<input type="text" class="form-control" placeholder="{{\'Search\' | r}} {{\'Products\' | r}}" ng-model="productSearchTerm"/>' +
+				'<i class="fa fa-search"></i>' +
+				'<span class="input-group-btn">' +
+				'<button type="submit" class="btn btn-default" ng-disabled="productSearchTerm == null || productSearchTerm == \'\'">{{\'Search\' | r}}</button>' +
+				'</span></div></div></form>'
+		);
+		$templateCache.put('productSearchNav.html',
+				'<style>.navbar .container .navbar-nav.pull-right li a i.fa-search,.navbar .container-view .navbar-nav.pull-right li a i.fa-search{border-radius:0;border:none;text-align:center;padding:0 10px;margin-top:0}.product-search-display{position:fixed;top:-100%;left:0;right:0;height:60px;background-color:#50acdb;padding:10px 5px;opacity:0;z-index:1030;transition:all 300ms;-moz-transition:all 300ms;-webkit-transition:all 300ms}.product-search-display.active{top:0;opacity:1;transition:all 300ms;-moz-transition:all 300ms;-webkit-transition:all 300ms}.product-search-display .fa-angle-double-up{cursor:pointer;margin-top:5px}</style>' +
+				'<div class="product-search-display" ng-class="{\'active\':displayProductSearch}">' +
+				'<div class="row">' +
+				'<div class="col-xs-10 col-md-11">' +
+				'<form name="productSearchInput" ng-submit="executeSearch()">' +
+				'<div class="view-form-icon">' +
+				'<div class="input-group" style="margin:0 0 10px;">' +
+				'<input type="text" class="form-control" placeholder="{{\'Search\' | r}} {{\'Products\' | r}}" ng-model="productSearchTerm"/>' +
+				'<i class="fa fa-search"></i>' +
+				'<span class="input-group-btn">' +
+				'<button type="submit" class="btn btn-default" ng-disabled="productSearchTerm == null || productSearchTerm == \'\'"><i class="fa fa-search"></i></button>' +
+				'</span></div></div></form></div>' +
+				'<div class="col-xs-2 col-md-1 text-center">' +
+				'<i class="fa fa-angle-double-up fa-2x" ng-click="displayProductSearch = false"></i>' +
+				'</div></div></div>' +
+				'<ul class="nav navbar-nav pull-right">' +
+				'<li class="search-toggle">' +
+				'<a href ng-click="displayProductSearch = true;">' +
+				'<i class="fa fa-search"></i></a></li></ul>'
+		);
+	}])
+;
---
+++ Product Search Input/README.md
@@ -0,0 +1,19 @@
+##Setup
+###1. Add module file to your project.
+Add the **`ProductSearchInput.js`** file to your project.
+
+If you are using a repository, add this file to the **`/lib`** directory.
+
+If you are using file overrides, create a new file override named **`lib/ProductSearchInput.js`** and add this file as the content for that file.
+
+**Be sure to reference both of these JS files in the `index.html` file**
+
+###2. Load the module into the application.
+Add a dependency for `OrderCloud-ProductSearchInput` to the Four51.app module in the **`js/app.js`** file.
+
+###3. Begin using the directives as you see fit!
+`<productsearchinput></productsearchinput>` can be placed anywhere in the application and it generates a simple text input group for searching products.
+
+`<productsearchinputnav></productsearchinputnav>` is meant to be used right before the shopping cart `ul` in the **`partials/controls/nav.html`** template.  This generates a search button to the left of the shopping cart nav item that the user can click on and the search input enters from the top of the screen.
+
+>Feel free to use these directives in tandem, but be careful to not duplicate functionality on the same view!  For instance, you could display the regular `<productsearchinput></productsearchinput>` directive on desktop views while giving users on mobile devices the `<productsearchinputnav></productsearchinputnav>` to conserve screen space.
