eb12cb47e27505b890ea3b89dd1a7b0bb07117e8
Connor OBrien <cobrien@four51.com>
2015-03-03 15:51:34 -0600

    Adding CKEditor to custom solutions repo

---
+++ CKEditor/ck.js
@@ -0,0 +1,30 @@
+angular.module('ck', [])
+
+    .directive('ckEditor', function() {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function(scope, elm, attr, ngModel) {
+                /* Replace whatever element this directive is on with a ck-editor */
+                var ck = CKEDITOR.replace(elm[0], {
+                    removeButtons: 'Source'
+                    /* remove or add any custom buttons as needed here */
+                });
+
+                /* Take no additional action if there is no ngModel value on the element */
+                if(!ngModel) return;
+
+                /* Directive detects that text has been pasted in from the clipboard */
+                ck.on('pasteState', function() {
+                    scope.$apply(function() {
+                        ngModel.$setViewValue(ck.getData());
+                    });
+                });
+
+                /* When the text is changed the ng-model value is updated */
+                ngModel.$render = function() {
+                    ck.setData(ngModel.$viewValue);
+                };
+            }
+        };
+    });
