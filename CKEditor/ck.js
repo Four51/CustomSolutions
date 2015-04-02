aa48a657b043cf5bde7e056a060fc84b1d421cb6
Connor OBrien <cobrien@four51.com>
2015-04-02 15:58:46 -0500

    Update so be consistent with other modules

--- CKEditor/ck.js
+++ CKEditor/ck.js
@@ -1,30 +1,33 @@
-angular.module('ck', [])
+angular.module('OrderCloud-CKEditor', []);
+angular.module('OrderCloud-CKEditor')
+    .directive('ckEditor', ckEditorDirective)
+;
 
-    .directive('ckEditor', function() {
-        return {
-            restrict: 'A',
-            require: '?ngModel',
-            link: function(scope, elm, attr, ngModel) {
-                /* Replace whatever element this directive is on with a ck-editor */
-                var ck = CKEDITOR.replace(elm[0], {
-                    removeButtons: 'Source'
-                    /* remove or add any custom buttons as needed here */
+function ckEditorDirective(){
+    return {
+        restrict: 'AE',
+        require: '?ngModel',
+        link: function(scope, elm, attr, ngModel) {
+            /* Replace whatever element this directive is on with a ck-editor */
+            var ck = CKEDITOR.replace(elm[0], {
+                removeButtons: 'Source'
+                /* remove or add any custom buttons as needed here */
+            });
+
+            /* Take no additional action if there is no ngModel value on the element */
+            if(!ngModel) return;
+
+            /* Directive detects that text has been pasted in from the clipboard */
+            ck.on('pasteState', function() {
+                scope.$apply(function() {
+                    ngModel.$setViewValue(ck.getData());
                 });
+            });
 
-                /* Take no additional action if there is no ngModel value on the element */
-                if(!ngModel) return;
-
-                /* Directive detects that text has been pasted in from the clipboard */
-                ck.on('pasteState', function() {
-                    scope.$apply(function() {
-                        ngModel.$setViewValue(ck.getData());
-                    });
-                });
-
-                /* When the text is changed the ng-model value is updated */
-                ngModel.$render = function() {
-                    ck.setData(ngModel.$viewValue);
-                };
-            }
-        };
-    });
+            /* When the text is changed the ng-model value is updated */
+            ngModel.$render = function() {
+                ck.setData(ngModel.$viewValue);
+            };
+        }
+    };
+}
