10e93996f2c0f47991904f32557b8a712f369210
jenrasmussen <jrasmussen@four51.com>
2015-04-16 09:26:52 -0500

    tested /  ready for use
    moved template into directive (per standards)

--- slideOutNav/slideOutNav.js
+++ slideOutNav/slideOutNav.js
@@ -8,39 +8,39 @@ angular.module('OrderCloud-SlideOutNav')
 function slideoutnav() {
     var directive = {
         restrict: 'E',
-        template: slideOutNavTemplate,
+        template: template,
         controller: 'SlideOutNavCtrl'
     };
     return directive;
-}
 
-function slideOutNavTemplate() {
-    return [
-        '<style>',
-        '.slideout-nav { border:1px solid #d1d2d4; border-radius:1px; border-right:0; position:fixed; top:70px; right:-300px; width:300px; background-color:#d1d2d4;' +
-            'padding:10px 10px 20px 20px; z-index:1040; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out; }',
-        '.slideout-nav.showNav { right:0; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out; }',
-        '.slideout-nav .row { min-height:60px; }',
-        '.slideout-nav .header { height:40px; border-bottom:1px solid #ccc; color:#c21c22; font-size:24px; text-transform:uppercase; font-weight:bold; margin-bottom: 30px; }',
-        '.slideout-nav img { float:left; padding:0 20px; max-width:95%; height:auto; }',
-        '</style>',
-        '<div class="slideout-nav text-left" ng-class="{\'showNav\':showSlideOutNav}">',
-        '<div class="header">',
-        '<span class="pull-left"><h5>SlideOut Nav</h5></span>',
-        '<a class="pull-right" ng-click="showSlideOutNav = false"><i class="fa fa-angle-double-right"></i></a>',
-        '</div>',
-        '<div class="row" ng-repeat="item in slideOutNavItems">',
-        '<span ng-if="item.Label != \'none\'">',
-        '<a target="_blank" href="{{item.externalLink}}">',
-        '<img ng-src="{{item.imageUrl}}" />',
-        '</a>',
-        '</span>',
-        '<span ng-if="item.Label == \'none\'">',
-        '<img ng-src="{{item.imageUrl}}" />',
-        '</span>',
-        '</div>',
-        '</div>'
-    ].join('');
+    function template() {
+        return [
+            '<style>',
+                '.slideout-nav { border:1px solid #d1d2d4; border-radius:1px; border-right:0; position:fixed; top:70px; right:-300px; width:300px; background-color:#d1d2d4;' +
+                'padding:10px 10px 20px 20px; z-index:1040; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out; }',
+            '.slideout-nav.showNav { right:0; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out; }',
+            '.slideout-nav .row { min-height:60px; }',
+            '.slideout-nav .header { height:40px; border-bottom:1px solid #ccc; color:#c21c22; font-size:24px; text-transform:uppercase; font-weight:bold; margin-bottom: 30px; }',
+            '.slideout-nav img { float:left; padding:0 20px; max-width:95%; height:auto; }',
+            '</style>',
+            '<div class="slideout-nav text-left" ng-class="{\'showNav\':showSlideOutNav}">',
+            '<div class="header">',
+            '<span class="pull-left"><h5>SlideOut Nav</h5></span>',
+            '<a class="pull-right" ng-click="showSlideOutNav = false"><i class="fa fa-angle-double-right"></i></a>',
+            '</div>',
+            '<div class="row" ng-repeat="item in slideOutNavItems">',
+            '<span ng-if="item.Label != \'none\'">',
+            '<a target="_blank" href="{{item.externalLink}}">',
+            '<img ng-src="{{item.imageUrl}}" />',
+            '</a>',
+            '</span>',
+            '<span ng-if="item.Label == \'none\'">',
+            '<img ng-src="{{item.imageUrl}}" />',
+            '</span>',
+            '</div>',
+            '</div>'
+        ].join('');
+    }
 }
 
 SlideOutNavCtrl.$inject = ['$scope','User'];
@@ -59,5 +59,4 @@ function SlideOutNavCtrl($scope, User) {
             }
         });
     })
-
 }
