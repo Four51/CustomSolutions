625fc2d7b9065366806d923377bded944d734b66
jenrasmussen <jrasmussen@four51.com>
2015-04-20 09:47:29 -0500

    Update moduleTemplate.js
    rearrange template order for consistency

--- Module Template/moduleTemplate.js
+++ Module Template/moduleTemplate.js
@@ -1,59 +1,58 @@
 angular.module('module-name', []);
 
 angular.module('module-name')
-
-    .factory('ServiceName', ServiceName)
-    .controller('ModuleCtrl', ModuleCtrl)
     .directive('directivename', directivename)
+    .controller('ModuleCtrl', ModuleCtrl)
+    .factory('ServiceName', ServiceName)
     .run(run)
     .config(config)
 ;
 
-    ServiceName.$inject = ['User'];
-    function ServiceName(User) {
-        var service = {
-            method: method
-        };
-        return service;
+function directivename() {
+    var directive = {
+        restrict: 'E',
+        template: template,
+        controller: ModuleCtrl
+    };
+    return directive;
 
-        function method() {
-
-        }
+    function template() {
+        return [
+            '<div>',
+                '<input type="text" ng-model="moduleTest" />',
+            '</div>'
+        ].join('');
     }
+}
 
-    ModuleCtrl.$inject = ['$scope', 'ServiceName'];
-    function ModuleCtrl($scope, ServiceName) {
-        $scope.moduleTest = "Hello World!";
+ModuleCtrl.$inject = ['$scope', 'ServiceName'];
+function ModuleCtrl($scope, ServiceName) {
+    $scope.moduleTest = "Hello World!";
 
-        ServiceName.method();
+    ServiceName.method();
+}
+
+ServiceName.$inject = ['User'];
+function ServiceName(User) {
+    var service = {
+        method: method
+    };
+    return service;
+
+    function method() {
+
     }
+}
 
-    function directivename() {
-        var directive = {
-            restrict: 'E',
-            template: template,
-            controller: ModuleCtrl
-        };
-        return directive;
+run.$inject = ['$templateCache'];
+function run($templateCache) {
+    $templateCache.put('moduleView.html',
+        '<div>{{moduleTest}}</div>'
+    );
+}
 
-        function template() {
-            return [
-                '<div>',
-                    '<input type="text" ng-model="moduleTest" />',
-                '</div>'
-            ].join('');
-        }
-    }
-
-    run.$inject = ['$templateCache'];
-    function run($templateCache) {
-        $templateCache.put('moduleView.html',
-            '<div>{{moduleTest}}</div>'
-        );
-    }
-
-    config.$inject = ['$routeProvider'];
-    function config($routeProvider) {
-        $routeProvider.
-            when('/pathname', { templateUrl: 'moduleView.html', controller: 'ModuleCtrl' })
-    }
+config.$inject = ['$routeProvider'];
+function config($routeProvider) {
+    $routeProvider.
+        when('/pathname', { templateUrl: 'moduleView.html', controller: 'ModuleCtrl' })
+}
