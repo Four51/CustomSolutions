30f238855b6a21ff24b6593d0125bcd11cbe31ac
mirandaposthumus <mirandaposthumus@users.noreply.github.com>
2015-04-30 09:45:36 -0500

    Update carousel.js
    added html here to eliminate the second file, updated names/formatting to match module pattern

--- Bootstrap Carousel/carousel.js
+++ Bootstrap Carousel/carousel.js
@@ -1,12 +1,41 @@
-angular.module('carousel', ['ngAnimate']);
-angular.module('carousel')
-    .directive('customCarousel', customCarousel)
+angular.module('OrderCloud-Carousel', ['ngAnimate']);
+
+angular.module('OrderCloud-Carousel')
+    .directive('customcarousel', customcarousel)
     .controller('customCarouselCtrl', customCarouselCtrl)
     .filter('intervalFilter', intervalFilter)
     .filter('carouselFilter', carouselFilter)
 ;
 
-customCarouselCtrl.$inject = ['$scope', '$animate', '$filter'];
+function customcarousel() {
+    var directive =  {
+        restrict: 'E',
+        template: template,
+        controller: customCarouselCtrl
+    };
+    return directive;
+
+    function template() {
+        return [
+            '<style>',
+            '.carousel-inner a[ng-href=""]:hover{cursor: default}',
+            '.carousel-control.left {background-image: none;}',
+            '.carousel-control.right {background-image: none;}',
+            '</style>',
+            '<carousel interval="myInterval">',
+            '<slide ng-repeat="slide in slides" active="slide.active">',
+            '<a style="max-width: 100%; height: auto;" ng-href="{{slide.link}}" target="{{slide.link.indexOf(\'http\') > -1 ? \'_blank\' : \'_self\'}}">',
+            '<img ng-src="{{slide.image}}">',
+            '</a>',
+            '<div class="carousel-caption">',
+            '<h4 ng-show="slide.text.toUpperCase() != \'NONE\'">{{slide.text}}</h4>',
+            '</div>',
+            '</slide>',
+            '</carousel>'
+        ].join('');
+    }
+}
+
 function customCarouselCtrl($scope, $animate, $filter) {
     $animate.enabled(false);
     $scope.slides = [];
@@ -18,14 +47,6 @@ function customCarouselCtrl($scope, $animate, $filter) {
 }
 customCarouselCtrl.$inject = ['$scope', '$animate', '$filter'];
 
-
-function customCarousel() {
-    return {
-        restrict: 'E',
-        templateUrl: 'partials/controls/carousel.html',
-        controller: customCarouselCtrl
-    };
-}
 
 function intervalFilter() {
     return function (fields, name) {
@@ -56,3 +77,5 @@ function carouselFilter() {
         return result;
     }
 }
+
+
