e686504748f4ec8a5adde08923ebce3a375ebae3
Jen Rasmussen <jrasmussen@four51.com>
2015-05-05 10:02:28 -0500

    Issue #9 - slideOut Navigation - need 3rd parameter for internal links to not open in new tab

--- slideOut Navigation/slideOutNav.js
+++ slideOut Navigation/slideOutNav.js
@@ -16,8 +16,8 @@ function slideoutnav() {
     function template() {
         return [
             '<style>',
-            '.slideout-nav {border:1px solid #d1d2d4; border-radius:1px; border-right:0; position:fixed; top:70px; right:-300px; width:300px; background-color:#d1d2d4;' +
-            'padding:10px 10px 20px 20px; z-index:1040; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out;}',
+                '.slideout-nav {border:1px solid #d1d2d4; border-radius:1px; border-right:0; position:fixed; top:70px; right:-300px; width:300px; background-color:#d1d2d4;' +
+                'padding:10px 10px 20px 20px; z-index:1040; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out;}',
             '.slideout-nav.showNav {right:0; transition:all 300ms ease-in-out; -webkit-transition:all 300ms ease-in-out;}',
             '.slideout-nav .row {min-height:60px;}',
             '.slideout-nav .header {height:40px; border-bottom:1px solid #ccc; color:#c21c22; font-size:24px; text-transform:uppercase; font-weight:bold; margin-bottom: 30px;}',
@@ -29,13 +29,18 @@ function slideoutnav() {
             '<a class="pull-right" ng-click="showSlideOutNav = false"><i class="fa fa-angle-double-right"></i></a>',
             '</div>',
             '<div class="row" ng-repeat="item in slideOutNavItems">',
-            '<span ng-if="item.linkUrl != \'none\'">',
-            '<a target="_blank" href="{{item.linkUrl}}">',
+            '<span ng-if="item.linkUrl == \'none\'">',
+            '<img ng-src="{{item.imageUrl}}" />',
+            '</span>',
+            '<span ng-if="item.linkUrl != \'none\' && item.externalLink == false">',
+            '<a href="{{item.linkUrl}}">',
             '<img ng-src="{{item.imageUrl}}" />',
             '</a>',
             '</span>',
-            '<span ng-if="item.linkUrl == \'none\'">',
+            '<span ng-if="item.externalLink == true">',
+            '<a target="_blank" href="{{item.linkUrl}}">',
             '<img ng-src="{{item.imageUrl}}" />',
+            '</a>',
             '</span>',
             '</div>',
             '</div>'
@@ -51,7 +56,8 @@ function SlideOutNavCtrl($scope, User) {
             if (f.Name.indexOf('SlideOutNav') > -1) {
                 var l = {
                     'imageUrl': f.File.Url,
-                    'linkUrl': f.Label
+                    'linkUrl': f.Label,
+                    'externalLink': f.Label.indexOf('http') > -1
                 };
                 $scope.slideOutNavItems.push(l);
             }
