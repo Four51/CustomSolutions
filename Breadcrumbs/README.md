16b0c8839ffe97a2224fb45e346c82fbc9c08446
Jen Rasmussen <jrasmussen@four51.com>
2016-05-03 12:01:04 -0500

    COMTOOLS-263 Breadcrumbs

---
+++ Breadcrumbs/README.md
@@ -0,0 +1,85 @@
+#Breadcrumbs for OrderCloud 
+
+This solution produces a breadcrumb of categories and sub categories as users navigate through the category tree.
+
+##Setup
+
+###1. Add the Breadcrumbs module file to your project
+
+Add the **`breadcrumbs.js`** file to your project.
+
+If you are using a repository, add this file to the **`/lib/oc`** directory.
+
+If you are using file overrides, create a new file override named **`lib/oc/breadcrumbs.js`** and add this file as the content by following these steps:
+
+ 1. Edit your 2.0 site
+ 2. Go to “Code Editor” tab
+ 3. Hit “New File Override”
+ 4. Name this file `lib/oc/breadcrumbs.js`
+ 5. Place raw code in the section below. Save.
+
+**Important!** Be sure to reference the new/updated JS file in the **`index.html`** file by following these steps:
+
+ 1. In Code Editor, locate your `index.html` file, hit edit.
+ 2. Add **`<script src="lib/oc/breadcrumbs.js" data-group="resources"></script>`** in the section with “lib/oc” files. Save.
+
+###2. Load the module into your application.
+Add a dependency for  **`OrderCloud-Breadcrumbs`** to the Four51.app module in the **`js/app.js`** file by following these steps: 
+
+ 1. In Code Editor, locate your **`js/app.js`** file, hit edit.
+ 2. Add **'OrderCloud-Breadcrumbs'** into the file. Save
+
+###3. Update the product.js file
+
+Update the **`product.js`** file.
+
+If you are using a repository, update the **`/js/directives/product.js`** file.
+
+If you are using file overrides, create a new file override for **`js/directives/product.js`** and comment out the below section:
+
+ 1. Edit your 2.0 site
+ 2. Go to “Code Editor” tab
+ 3. Hit “New File Override”
+ 4. Name this file `js/directives/product.js`
+ 5. Comment out the directive listed below and save.
+
+ ```javascript
+ four51.app.directive('productnav', function() {
+	var obj = {
+		scope: {
+			product: '=',
+			variant: '=',
+			editvariant: '='
+		},
+		restrict: 'E',
+		templateUrl: 'partials/controls/productNav.html'
+	};
+	return obj;
+});
+```
+
+###4. Add the 'breadcrumbs' directive
+
+ 1. In Code Editor, locate your `partials/categoryView.html` file, hit edit.
+ 2. Add the breadcrumbs directive as shown below in the “partials/categoryView.html” file. Save.
+
+```html
+    <nav id="451qa_nav_hdr" class="nav">
+        <ul>
+            <li class="hidden-sm hidden-xs">
+                <span ng-click="toggleNav()"><i class="fa fa-bars" ng-class="{ 'text-info': navStatus.visible }"></i></span>
+            </li>
+            <li>
+                <span id="451_lbl_curcat" class="text-primary" ng-bind-html="currentCategory.Name || '  {{'Categories' | r | xlat}}'"></span>
+            </li>
+            <li>
+                <breadcrumbs></breadcrumbs>
+            </li>
+            <li class="pull-right">
+                <a href="search">
+                    <span><i class="fa fa-search text-primary"></i></span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+```
---
+++ Breadcrumbs/breadcrumbs.js
@@ -0,0 +1,132 @@
+angular.module('OrderCloud-Breadcrumbs', []);
+
+angular.module('OrderCloud-Breadcrumbs')
+    .directive('breadcrumbs', breadcrumbs)
+    .controller('BreadcrumbsCtrl', BreadcrumbsCtrl)
+    .directive('productnav', productnav)
+;
+
+function breadcrumbs() {
+    var directive =  {
+        restrict: 'E',
+        template: template,
+        controller: BreadcrumbsCtrl
+    };
+    return directive;
+
+    function template() {
+        return [
+            '<style>',
+            '.breadcrumbs {margin-left: 5px;}',
+            '.breadcrumb {margin-bottom:0px;padding:14px 15px;background-color: transparent;}',
+            '.breadcrumb a{color: #333333;}',
+            '.breadcrumbs li:last-child a{font-weight:bold;}',
+            '</style>',
+            '<div class="breadcrumbs" ng-show="breadcrumbs">',
+            '<ul class="breadcrumb">',
+            '<li ng-repeat="crumb in breadcrumbs">',
+            '<a ng-href="{{crumb.link}}">{{crumb.name}}</a>',
+            '</li>',
+            '</ul>',
+            '</div>'
+        ].join('');
+    }
+}
+
+BreadcrumbsCtrl.$inject = ['$scope','$cookieStore'];
+function BreadcrumbsCtrl($scope, $cookieStore) {
+
+    $scope.linkedTree = {};
+    $scope.breadcrumbs = [];
+    $cookieStore.put("breadCookie", $scope.breadcrumbs);
+
+    $scope.$watch('currentCategory', function(newVal) {
+        if (!newVal) return;
+        $scope.breadcrumbs = [];
+        initTree();
+        getNode($scope.linkedTree, $scope.currentCategory, $scope.breadcrumbs);
+    });
+
+    function initTree() {
+        $scope.linkedTree.Description = '';
+        $scope.linkedTree.Image = null;
+        $scope.linkedTree.Name = 'Catalog';
+        $scope.linkedTree.ProductViewName = null;
+        $scope.linkedTree.SortOptions = null;
+        $scope.linkedTree.InteropID = 'catalog';
+        $scope.linkedTree.Parent = null;
+        $scope.linkedTree.SubCategories = $scope.tree;
+        linkTree($scope.linkedTree.SubCategories, $scope.linkedTree);
+    }
+
+    function linkTree(currentNodes, parentNode) {
+        if (currentNodes) {
+            angular.forEach(currentNodes, function(node) {
+                node.Parent = parentNode;
+                linkTree(node.SubCategories, node);
+            });
+        }
+    }
+
+    function getNode(currentNode, node, breadcrumbs) {
+        if (currentNode.InteropID === node.InteropID) {
+            getBreadCrumbs(currentNode, breadcrumbs);
+        }
+        else if (currentNode.SubCategories) {
+            angular.forEach(currentNode.SubCategories, function(cat) {
+                getNode(cat, node, breadcrumbs);
+            });
+        }
+    }
+
+    function getBreadCrumbs(node, breadcrumbs) {
+        if (node) {
+            var linkPath;
+            if (node.InteropID !== 'catalog') {
+                linkPath = 'catalog/' + node.InteropID;
+            } else {
+                linkPath = node.InteropID;
+            }
+            breadcrumbs.unshift({name: node.Name, link: linkPath});
+            $cookieStore.put("breadCookie", breadcrumbs);
+            getBreadCrumbs(node.Parent, breadcrumbs);
+        }
+    }
+}
+
+function productnav() {
+    var directive = {
+        scope: {
+            product: '=',
+            variant: '=',
+            editvariant: '='
+        },
+        restrict: 'E',
+        template: template,
+        controller: ['$scope','$cookieStore', function($scope,$cookieStore){
+            $scope.breadCookie = $cookieStore.get("breadCookie");
+        }]
+    };
+
+    return directive;
+
+    function template() {
+        return [
+            '<style>',
+            '.breadcrumbs {margin-left: 5px;}',
+            '.breadcrumb {margin-bottom:0px;padding:14px 15px;background-color: transparent;}',
+            '.breadcrumb a{color: #333333;}',
+            '.breadcrumbs li:last-child a{font-weight:bold;}',
+            '</style>',
+            '<div class="breadcrumbs" ng-show="breadCookie">',
+            '<ul class="breadcrumb">',
+            '<li ng-repeat="crumb in breadCookie">',
+            '<a ng-href="{{crumb.link}}">{{crumb.name}}</a>',
+            '</li>',
+            '<li><a href="product/{{product.InteropID}}">{{product.Name}}</a></li>',
+            '<li ng-if="variant && variant.ExternalID"><a href="product/{{product.InteropID}}/{{variant.InteropID}}">{{variant.ExternalID}}</a></li>',
+            '</ul>',
+            '</div>'
+        ].join('');
+    }
+}
